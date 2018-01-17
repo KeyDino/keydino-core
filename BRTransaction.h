20782389feb0a9a53e4f5b9a0266b944574d9259
BrendanEMahon <brendan.e.mahon@gmail.com>
2018-01-17 18:37:55 -0500

    Updating some hard coded fee parameters with temporary replacements

--- BRTransaction.h
+++ BRTransaction.h
@@ -34,13 +34,13 @@
 extern "C" {
 #endif
 
-#define TX_FEE_PER_KB        1000ULL     // standard tx fee per kb of tx size, rounded up to nearest kb
+#define TX_FEE_PER_KB        5ULL     // standard tx fee per kb of tx size, rounded up to nearest kb. Changed from 1000ULL to 5ULL for Bitcoin Cash. May be unreasonable.
 #define TX_OUTPUT_SIZE       34          // estimated size for a typical transaction output
 #define TX_INPUT_SIZE        148         // estimated size for a typical compact pubkey transaction input
 #define TX_MIN_OUTPUT_AMOUNT (TX_FEE_PER_KB*3*(TX_OUTPUT_SIZE + TX_INPUT_SIZE)/1000) //no txout can be below this amount
 #define TX_MAX_SIZE          1000000      // no tx can be larger than this size in bytes. Updated to 1MB for Bitcoin (Cash)
     //Now that Bitcoin fees are very low again, these parameters can be revisited
-#define TX_FREE_MAX_SIZE     1000        // tx must not be larger than this size in bytes without a fee
+#define TX_FREE_MAX_SIZE     100000        // tx must not be larger than this size in bytes without a fee.  Upped from 1000 to 100000 for Bitcoin Cash
 #define TX_FREE_MIN_PRIORITY 57600000ULL // tx must not have a priority below this value without a fee
 #define TX_UNCONFIRMED       INT32_MAX   // block height indicating transaction is unconfirmed
 #define TX_MAX_LOCK_HEIGHT   500000000   // a lockTime below this value is a block height, otherwise a timestamp
