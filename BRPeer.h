22f11a914338e4077e81856bae8dc2e44a505ace
BrendanEMahon <brendan.e.mahon@gmail.com>
2018-01-17 00:07:15 -0500

    transferred btc to bch in source code

--- BRPeer.h
+++ BRPeer.h
@@ -62,8 +62,8 @@ extern "C" {
 #define SERVICES_NODE_BLOOM   0x04 // BIP111: https://github.com/bitcoin/bips/blob/master/bip-0111.mediawiki
 #define SERVICES_NODE_BCH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
     
-#define BR_VERSION "0.6.2"
-#define USER_AGENT "/keydino:" BR_VERSION "/"
+#define BR_VERSION "0.0.1"
+#define USER_AGENT "/KeyDino:" BR_VERSION "/"
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
 #define MSG_VERSION     "version"
